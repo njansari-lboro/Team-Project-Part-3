17dcb64ccef82a75908ea1df889dd0163171c805
Nayan Jansari <nayanjansari@gmail.com>
2024-04-20 11:24:46 +0100

    Added doc comments

--- src/api/database/chat-db-helpers.php
+++ src/api/database/chat-db-helpers.php
@@ -2,11 +2,42 @@
     $db_name = "chat_system";
     require_once(__DIR__ . "/base-db-helpers.php");
 
-    function get_chat(int $id): ?object {
-        $sql = "SELECT * FROM chat WHERE id = :id";
-        return get_record($sql, "i", $id);
+    // FETCHING CHATS
+
+    /**
+     * Fetches the chat with the specified ID from the database.
+     *
+     * @param int $chat_id The ID of the chat to be fetched.
+     *
+     * @return ?object Returns the chat as an object, if found, else `null`.
+     *
+     * Usage example:
+     * ```
+     * $chat = get_chat(5);
+     * echo $chat->name; // "Team 12"
+     * ```
+     */
+    function get_chat(int $chat_id): ?object {
+        $sql = "SELECT * FROM chat WHERE id = ?";
+        return get_record($sql, "i", $chat_id);
     }
 
+    /**
+     * Fetches the chats from the database filtered using the specified properties.
+     *
+     * @param ?string $filter_text [optional] A filter string that matches a chat based on its name.
+     *
+     * @return array An array of chats as objects.
+     *
+     * Usage example:
+     * ```
+     * $all_chats = fetch_chats();
+     *
+     * // Filter chats on whether their name contains "team"
+     * $chats = fetch_chats(filter_text: "team");
+     * echo $chats; // [(Team 12), (The A-Team)]
+     * ```
+     */
     function fetch_chats(?string $filter_text = null): array {
         $sql = "SELECT * FROM chat WHERE 1";
 
