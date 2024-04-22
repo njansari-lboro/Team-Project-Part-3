befa5d4f41879fdb0bf16baea27ccbdbba076537
Nayan Jansari <nayanjansari@gmail.com>
2024-04-22 14:06:49 +0100

    Updated messages.php to use db helpers

--- src/api/chat/messages.php
+++ src/api/chat/messages.php
@@ -21,3 +21,50 @@
 
     $chat_id = $_GET["chat_id"] ?? null;
     $message_id = $_GET["message_id"] ?? null;
+
+    switch ($method) {
+    case "GET":
+        if ($chat_id === null) {
+            http_response_code(400);
+        } else {
+            if ($message_id === null) {
+                echo json_encode(fetch_messages(chat_id: $chat_id));
+            } else {
+                echo json_encode(get_message($message_id));
+            }
+        }
+
+        break;
+
+    case "POST":
+        $author_id = $_POST["author_id"] ?? null;
+        $body = $_POST["body"] ?? null;
+
+        if ($chat_id !== null && $message_id === null && $author_id !== null && $body !== null) {
+            add_message($chat_id, $author_id, $body);
+        } else {
+            http_response_code(400);
+        }
+
+        break;
+
+    case "PUT":
+        $body = $_POST["body"] ?? null;
+
+        if ($chat_id === null || $message_id === null) {
+            http_response_code(400);
+        } else {
+            update_message($message_id, $body);
+        }
+
+        break;
+
+    case "DELETE":
+        if ($chat_id === null || $message_id === null) {
+            http_response_code(400);
+        } else {
+            delete_message($message_id);
+        }
+
+        break;
+    }
