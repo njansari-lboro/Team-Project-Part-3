12682e26c2ad17f6fbc77d68d45abf966063b943
Krina Vara <kvara2003@hotmail.com>
2024-04-26 17:29:54 +0100

    check

--- src/pages/analytics/analytics.php
+++ src/pages/analytics/analytics.php
@@ -68,8 +68,5 @@
     <div id = "taskspiechart2" class = "col-sm"></div>
     </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </body>
 </html>
