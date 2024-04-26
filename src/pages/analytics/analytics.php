93dab8004c0d266b9965b7d026b17e140054b99b
Krina Vara <kvara2003@hotmail.com>
2024-04-26 16:04:57 +0100

    correction

--- src/pages/analytics/analytics.php
+++ src/pages/analytics/analytics.php
@@ -21,19 +21,20 @@
         <script src = "https://www.gstatic.com/charts/loaders.js"></script>
         <script>
         google.charts.load('current', {'packages':['corechart']});
-        google.charts.setOnLoadCallBack(drawProjectPieChart);
+        google.charts.setOnLoadCallback(drawProjectPieChart);
         function drawProjectPieChart(){
-            var project1Data = google.visualisation.arrayToDataTable([
+            var project1Data = google.visualization.arrayToDataTable([
+                ['Task', 'Count'],
                 ['Completed',5],
                 ['Uncompleted',2],
                 ['In Process', 1]]
                 );
-            var optionstitle = {
-                title: Project 1
+            var optionsTitle = {
+                title: 'Project 1'
 
             };
-            var taskpiechart = new google.visualisation.PieChart(document.getElementById('taskspiechart'));
-            taskpiechart.draw(project1Data, optionstitle);
+            var taskpiechart = new google.visualization.PieChart(document.getElementById('taskspiechart'));
+            taskpiechart.draw(project1Data, optionsTitle);
         }
         </script>
     </body>
