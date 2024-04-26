6616f7da4d91833eadb45b3d112c2d387bcf3b41
Krina Vara <kvara2003@hotmail.com>
2024-04-26 15:33:51 +0100

    adding chart

--- src/pages/analytics/analytics.php
+++ src/pages/analytics/analytics.php
@@ -18,5 +18,23 @@
     <body>
         <p class="some-text">This is the analytics.</p>
         <p class="some-text">This is the analytics.</p>
+        <script src = "https://www.gstatic.com/charts/loaders.js"></script>
+        <script>
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallBack(drawProjectPieChart);
+        function drawProjectPieChart(){
+            var project1Data = google.visualisation.arrayToDataTable([
+                ['Completed',5],
+                ['Uncompleted',2],
+                ['In Process', 1]]
+                );
+            var optionstitle = {
+                title: Project 1
+
+            };
+            var taskpiechart = new google.visualisation.PieChart(document.getElementById('taskspiechart'));
+            taskpiechart.draw(project1Data, optionstitle);
+        }
+        </script>
     </body>
 </html>
