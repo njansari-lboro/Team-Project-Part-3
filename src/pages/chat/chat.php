4538797a1d08ecca2bb56214b54af2d93c3de81f
Chis-code <155032827+Chis-code@users.noreply.github.com>
2024-04-29 14:44:27 +0100

    Update chat.php
    Added main body of the chat subsystem which just includes the different sections, (the left hand side to show recent messages, the container the show the actual chat between two people or group, and the top bar that will say which chat the user is currently in

--- src/pages/chat/chat.php
+++ src/pages/chat/chat.php
@@ -16,6 +16,19 @@
     </head>
 
     <body>
-        <p class="some-text">This is the chat.</p>
+        <div id="wrapper"><!--start of wrapper-->
+        <div id="left_pannel">
+            <div class="sidebar-title" style="text-align:center;"> My Messages  <i class="ri-message-2-line"></i> </div>
+                <form action="" class="sidebar-search">
+                    <input type="search" placeholder="search" class="sidebar-input"><!--making the type earch means an x will show up to delete what you were typing-->
+                    <button type="submit" class="sidebar-submit"><i class="ri-search-line"></i></button>
+                </form>
+        </div>
+        <div id="right_pannel">
+            <div id="header"></div>
+            <div id="container">
+            </div><!--end of container-->
+        </div>
+    </div> <!--end of wrapper-->
     </body>
 </html>
