38d8c041971dc7e30282cfb7e93ed638a8ebb161
Mark <147945241+StiltonCheese@users.noreply.github.com>
2024-05-02 11:32:26 +0100

    Update data analytics2.php

--- src/api/analytics/data analytics2.php
+++ src/api/analytics/data analytics2.php
@@ -1,14 +1,15 @@
 <?php
-function individualData(jsonData){
-	$dataToAnalyse = json_decode(jsonData);
+function individualData($jsonData){
+	$dataToAnalyse = json_decode($jsonData);
 	$totalHoursSpent = 0;
 	foreach($dataToAnalyse as $task){
 		$totalHoursSpent += $task['hours_spent'];
+	}
 	$returnData = $totalHoursSpent;
 	return $returnData;
 }
-function projectData(jsonData){
-	$dataToAnalyse = json_decode(jsonData);
+function projectData($jsonData){
+	$dataToAnalyse = json_decode($jsonData);
 	$totalTasks = count($dataToAnalyse);
 	$completeTasks = 0;
 	$inProgTasks = 0;
@@ -22,11 +23,7 @@ function projectData(jsonData){
 			$unstartedTasks++;
 		}
 	}
-	$returnData = json_encode({'total_tasks':$totalTasks,'complete_tasks':$completeTasks,'in_progress_tasks':$inProgTasks,'non-started_tasks':$unstartedTasks})
+	$returnData = json_encode(array('total_tasks'=>$totalTasks,'complete_tasks'=>$completeTasks,'in_progress_tasks'=>$inProgTasks,'non-started_tasks'=>$unstartedTasks));
 	return $returnData;
 }
-//no tasks
-//no comp tasks
-// no in prog tasks
-// no unstarted tasks
 ?>
