9c469f7d4b04fb02b09d0625eec0e86682ac2977
Chis-code <155032827+Chis-code@users.noreply.github.com>
2024-05-02 12:35:00 +0100

    Update chat.php
    added link for icons

--- src/pages/chat/chat.php
+++ src/pages/chat/chat.php
@@ -11,6 +11,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet"/>
 
         <link rel="stylesheet" href="chat/chat.css">
     </head>
