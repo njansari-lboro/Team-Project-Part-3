1a94cede1b7fae3229b88b905ba2ad1fd14bc1fb
Nayan Jansari <nayanjansari@gmail.com>
2024-05-03 17:04:25 +0100

    Refactored chat functions

--- src/pages/chat/chat-functions.php
+++ src/pages/chat/chat-functions.php
@@ -1,39 +1,13 @@
 <?php
-    $task = $_GET["task"] ?? null;
+    include_once(__DIR__ . "/../../database/users-db-helpers.php");
 
-    if ($task) {
-        include_once(__DIR__ . "/../../database/users-db-helpers.php");
+    $task = $_GET["task"] ?? die();
 
-        switch ($task) {
-        case "get_user":
-            $user_id = $_POST["user_id"];
-            $user = get_user($user_id);
-            $user->profile_image_path = get_user_pfp($user);
+    switch ($task) {
+    case "get_user":
+        $user_id = $_POST["user_id"];
+        $user = get_user($user_id);
 
-            echo json_encode($user);
-
-            break;
-
-        case "get_chat_icon":
-            $icon_name = $_POST["icon_name"] ?? null;
-
-            $path = null;
-
-            if ($icon_name) {
-                $absolute_path = realpath(__DIR__ . "/../../uploads/chat-icon-images/$icon_name");
-                $path = "/" . trim(str_replace($_SERVER["DOCUMENT_ROOT"], "", $absolute_path), "/");
-            }
-
-            echo json_encode($path);
-
-            break;
-        }
-
-        exit();
-    }
-
-    // Gets the full path (from root) of the specified user's profile image
-    function get_user_pfp(object $user): ?string {
         $path = null;
         $name = $user->profile_image_name;
 
@@ -42,5 +16,23 @@
             $path = "/" . trim(str_replace($_SERVER["DOCUMENT_ROOT"], "", $absolute_path), "/");
         }
 
-        return $path;
+        $user->profile_image_path = $path;
+
+        echo json_encode($user);
+
+        break;
+
+    case "get_chat_icon":
+        $icon_name = $_POST["icon_name"] ?? null;
+
+        $path = null;
+
+        if ($icon_name) {
+            $absolute_path = realpath(__DIR__ . "/../../uploads/chat-icon-images/$icon_name");
+            $path = "/" . trim(str_replace($_SERVER["DOCUMENT_ROOT"], "", $absolute_path), "/");
+        }
+
+        echo json_encode($path);
+
+        break;
     }
